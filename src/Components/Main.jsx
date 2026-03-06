56a7ca7c20cb75c9e2b9876aa70384e235913231
Nagendra476 <Nagendranani4431@gmail.com>
2026-03-06 19:00:01 +0530

    first

--- src/Components/Main.jsx
+++ src/Components/Main.jsx
@@ -2,11 +2,9 @@ import { Routes,Route } from 'react-router-dom';
 import React from 'react';
 import Home from '../Components/Home.jsx';
 import SeatHall from '../Components/SeatHall.jsx';
-import Login from '../Components/Login.jsx';
-import Practice from '../Components/Practice.jsx';
-import Navbar2 from '../Components/Navbar2.jsx';
-import Prop from '../Components/Prop.jsx';
-import Flag from '../Components/Flag.jsx';
+
+
+
 {/*
 import PopularSection from './Components/PopularSection';
 import AppPromo from './Components/AppPromo';
@@ -20,11 +18,7 @@ function App() {
           <Routes>
             <Route path='/' element={<Home />} />
             <Route path='/seat-hall/:id' element={<SeatHall />} />
-            <Route path='/login' element={<Login />} />
-            <Route path='/practice' element={<Practice />} />
-            <Route path='/navbar2' element={<Navbar2 />} />
-            <Route path='/employee' element={<Prop />} />
-            <Route path='/flag' element={<Flag />} />
+            
           </Routes>  
       </div>
 
